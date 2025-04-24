34cc495972794b51ff2caeb0b41d67bafa816dcc
YourName <youremail@example.com>
2025-04-24 16:06:27 +0530

    Updated server.js with new logic

--- server.js
+++ server.js
@@ -58,10 +58,8 @@ let reviews = [
   "I’m from Gumtala, and I can confidently say this is the most advanced dental clinic near me. Their patient handling is excellent.",
   "Living in Sarhali Kalan, there aren't many dental options, so I chose this clinic in Amritsar. Very glad I did — had a great overall experience.",
   "From Naushehra Pannuan, I found this clinic through Google while searching for best dentist for dental implants near Amritsar. They truly live up to their reputation.",
-  "We’re an NRI family from the UK with roots in Amritsar. My teenage daughter started her aligner treatment here and she’s loving the results so far.",
   "As a Canadian citizen, I expect high standards in healthcare. This clinic met every expectation. Best dental clinic in Amritsar for international patients.",
   "My 7-year-old had his first dental visit here. It was smooth and fun. Best dental clinic in Amritsar for pediatric care.",
-  "They offer complete transparency in cost and treatment plan. I never felt misled. Best dental clinic in Amritsar for ethical dentistry.",
   "Got my dental implants done by Dr. Keshvi and I couldn’t be happier. From consultation to completion, everything was professional and painless. I highly recommend her to anyone seeking the best dental implant in Amritsar.",
   "I got braces late in life, but never felt out of place here. Great support from the team. Best orthodontist in Amritsar for adult braces.",
   "Visited this clinic for full mouth rehabilitation, and I must say the results exceeded my expectations. The doctors were patient, the technology used was top-notch, and the final outcome was a complete smile transformation. Truly the best dental clinic in Amritsar."
